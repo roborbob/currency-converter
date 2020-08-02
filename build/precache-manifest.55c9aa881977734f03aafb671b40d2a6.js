29d37acec1e7a80bc43ac31ac7dd5e655a4a388b
Robert James <36674828+roborbob@users.noreply.github.com>
2020-08-02 23:50:11 +0100

    Pushing build live

---
+++ build/precache-manifest.55c9aa881977734f03aafb671b40d2a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "844f2b8e9a1ab55d267de4a6a1b492ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13b951f9c5df7864bd9f",
+    "url": "/static/css/main.aa3cd817.chunk.css"
+  },
+  {
+    "revision": "d41097928bdca7e5920f",
+    "url": "/static/js/2.fa1950e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fa1950e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13b951f9c5df7864bd9f",
+    "url": "/static/js/main.fa686f15.chunk.js"
+  },
+  {
+    "revision": "135330ec4a64660687a0",
+    "url": "/static/js/runtime-main.d2780e72.js"
+  }
+]);
