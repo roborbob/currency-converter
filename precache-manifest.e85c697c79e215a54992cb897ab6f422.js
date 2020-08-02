0067801e75907c24ecb324b00c3515969793df09
Robert James <36674828+roborbob@users.noreply.github.com>
2020-08-03 00:11:05 +0100

    Updates

---
+++ precache-manifest.e85c697c79e215a54992cb897ab6f422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2edf625fa6c49b42d04a511a43b10cf",
+    "url": "/currency-converter/index.html"
+  },
+  {
+    "revision": "b332d28becb7632d3401",
+    "url": "/currency-converter/static/css/main.aa3cd817.chunk.css"
+  },
+  {
+    "revision": "f2ca6a020c9c1c910548",
+    "url": "/currency-converter/static/js/2.92a5b76e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/currency-converter/static/js/2.92a5b76e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b332d28becb7632d3401",
+    "url": "/currency-converter/static/js/main.aa78508b.chunk.js"
+  },
+  {
+    "revision": "a0730edf8381b13d6dc3",
+    "url": "/currency-converter/static/js/runtime-main.46121f8e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.55c9aa881977734f03aafb671b40d2a6.js"
+  "/currency-converter/precache-manifest.e85c697c79e215a54992cb897ab6f422.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/currency-converter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
